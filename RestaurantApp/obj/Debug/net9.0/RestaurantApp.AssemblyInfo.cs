419a2d93c3eb740b0c62d4e466e48ada50489b03
Siberian <vasileios.kritikos@aueb.gr>
2025-04-16 11:29:07 +0300

    migration

--- RestaurantApp/obj/Debug/net9.0/RestaurantApp.AssemblyInfo.cs
+++ RestaurantApp/obj/Debug/net9.0/RestaurantApp.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestaurantApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d757fac7537471dcec2e166fbea24ba8d698bfd7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82612f472df150ae2482cbf87c042bd36700f3b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestaurantApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RestaurantApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
